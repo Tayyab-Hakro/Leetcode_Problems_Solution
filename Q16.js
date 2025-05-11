bed639c31895cb68322b860049649193c066c95f
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-11 04:01:29 -0700

    question solved

--- Q16.js
+++ Q16.js
@@ -1,8 +1,15 @@
-let arr = [1,2,3]
-let res = []
-for(let i = 0 ;i <arr.length ;i++){
-    for(let j = i ; j<arr.length ;j++){
-        res.push([arr[j]])
-    }
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+ let freq ={}
+ for(let char of arr){
+    freq[char] =(freq[char] || 0) +1
+ }
+ let val = Object.values(freq)
+ let uique = new Set(val)
+return uique.size  === val.length
 }
-console.log(res)
+console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3]))
+console.log(uniqueOccurrences([1,2]))
