2b3a3dcf76ac0ef87723fc6681974930e83e61e8
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-11 02:29:10 -0700

    working on 11/5/2025 Sunday

--- Q14.js
+++ Q14.js
@@ -1,3 +1,24 @@
-let a = "this is new"
-let b = "Let's become winner"
-console.log(a + " " + b)
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function(words) {
+    const result = [];
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words.length; j++) {
+            if (i !== j && words[j].includes(words[i])) {
+                result.push(words[i]);
+                break; // No need to check further once a match is found
+            }
+        }
+    }
+
+    return result;
+};
+
+console.log(stringMatching(["mass","as","hero","superhero"]))
