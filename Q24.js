d20b5f4b0f0eb2a90f63bb0b8193d9252d60edde
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-14 02:44:50 -0700

    learning

--- Q24.js
+++ Q24.js
@@ -1,31 +1,14 @@
 var countNegatives = function(grid) {
-    let count = 0;
-    let rows = grid.length;
-    let cols = grid[0].length;
-    console.log(cols)
-    for (let i = 0; i < rows; i++) {
-        let low = 0;
-        let high = cols - 1;
-        
-        while (low <= high) {
-            let mid = Math.floor((low + high) / 2);
-            
-            if (grid[i][mid] < 0) {
-                high = mid - 1; // look in the left half
-            } else {
-                low = mid + 1;  // look in the right half
-            }
+    let n = grid.length;
+    let x = 0, i = grid[0].length - 1;
+    let total = 0;
+    while(i >= 0 && x < n) {
+        if(grid[x][i] < 0) {
+            total += n - x;
+            i--;
+        } else {
+            x++;
         }
-        
-        // all elements from index `low` to end are negative
-        count += (cols - low);
     }
-    
-    return count;
+    return total;
 };
-console.log(countNegatives([
-  [4, 3, 2, -1],
-  [3, 2, 1, -1],
-  [1, 1, -1, -2],
-  [-1, -1, -2, -3]
-]));
