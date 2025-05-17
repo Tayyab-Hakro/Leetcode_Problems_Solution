ada53bcb359e9779bfd21d78f26ad9258e5e554e
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-17 02:17:40 -0700

    17/5/2025

---
+++ Q32.js
@@ -0,0 +1,21 @@
+function maxSum(arr, k) {
+  let max = 0;
+  let windowSum = 0;
+
+  // Calculate the first window sum
+  for (let i = 0; i < k; i++) {
+    windowSum += arr[i];
+  }
+
+  max = windowSum;
+
+  // Slide the window
+  for (let i = k; i < arr.length; i++) {
+    windowSum += arr[i] - arr[i - k];
+    max = Math.max(max, windowSum);
+  }
+
+  return max;
+}
+
+console.log(maxSum([2, 1, 5, 1, 3, 2], 3)); // Output: 9 (5+1+3)
